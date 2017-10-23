abf8d8b1f709e541bbf29f458b02ecc6ac6b06d3
okursofware <okurokur2014@gmail.com>
2017-10-23 01:38:17 -0700

    versiyon0.2

--- my_laravel3/resources/views/welcome.blade.php
+++ my_laravel3/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Muhammed OKUR</title>
+        <title>MMS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Muhammed Bey Hoşgeldiniz    
+                    Laravel Hoşgeldiniz 2017    
                 </div>
 
                 <div class="links">
